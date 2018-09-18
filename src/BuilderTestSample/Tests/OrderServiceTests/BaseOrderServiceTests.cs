8acabf2b34389cd1faface71c7211839e703ca21
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-18 15:09:30 -0400

    Splitting tests by exception type

---
+++ src/BuilderTestSample/Tests/OrderServiceTests/BaseOrderServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using BuilderTestSample.Services;
+using BuilderTestSample.Tests.TestBuilders;
+
+namespace BuilderTestSample.Tests.OrderServiceTests
+{
+    public abstract class BaseOrderServiceTests
+    {
+        protected readonly OrderService _orderService = new OrderService();
+        protected readonly OrderBuilder _orderBuilder = new OrderBuilder();
+        protected readonly CustomerBuilder _customerBuilder = new CustomerBuilder();
+    }
+}
---
+++ src/BuilderTestSample/Tests/OrderServiceTests/PlaceOrderThrowsInvalidOrder.cs
@@ -0,0 +1,41 @@
+﻿using BuilderTestSample.Exceptions;
+using Xunit;
+
+namespace BuilderTestSample.Tests.OrderServiceTests
+{
+    public class PlaceOrderThrowsInvalidOrder : BaseOrderServiceTests
+    {
+        [Fact]
+        public void ThrowsException_GivenOrderWithExistingId()
+        {
+            var order = _orderBuilder
+                .WithTestValues()
+                .Id(123)
+                .Build();
+
+            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+        }
+
+        [Fact]
+        public void ThrowsException_GivenOrderAmountOfZero()
+        {
+            var order = _orderBuilder
+                .WithTestValues()
+                .OrderAmount(0)
+                .Build();
+
+            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+        }
+
+        [Fact]
+        public void ThrowsException_GivenOrderWithoutCustomer()
+        {
+            var order = _orderBuilder
+                .WithTestValues()
+                .Customer(null)
+                .Build();
+
+            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+        }
+    }
+}
